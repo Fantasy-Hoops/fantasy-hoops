6322051aa093390542356b28bc28f0c8a212d5e7
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-02-14 22:18:34 +0200

    Main page images by team

--- ClientApp/src/components/Main.jsx
+++ ClientApp/src/components/Main.jsx
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
+import { isAuth, parse } from '../utils/auth';
 
 export class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      team: ''
+    }
+  }
+
+  async componentWillMount() {
+    if (isAuth()) {
+      const user = parse();
+      await fetch(`http://fantasyhoops.org/api/user/team/${user.id}`)
+        .then(res => res.json())
+        .then(res => {
+          const team = res[0].team;
+          this.setState({
+            team: team,
+          });
+        });
+    } else {
+      this.setState({
+        team: { abbreviation: 'nba' }
+      })
+    }
+  }
+
   render() {
+    if (this.state.team === '') {
+      return <div></div>;
+    }
+    const image = require(`../content/images/backgrounds/${this.state.team.abbreviation.toLowerCase()}.png`);
     return (
       <div>
-        <div>
-          <p id="main-text" className="text-center title" style={{ marginTop: '15%' }}>
-            Fantasy Hoops
+        <p id="main-text" className="text-center title" style={{ marginTop: '15%' }}>
+          Fantasy Hoops
           </p>
-          <div className="text-center">
-            <a href="/lineup" className="btn btn-primary mt-4" role="button" style={{ width: '20%', fontSize: '80%' }}>
-              Play Now!
+        <div className="text-center">
+          <a href="/lineup" className="btn btn-primary mt-4" role="button" style={{ width: '20%', fontSize: '80%' }}>
+            Play Now!
             </a>
-          </div>
         </div>
-        <video muted autoPlay loop id="main-video">
-          <source src={require("../content/videos/mainVideo.mp4")} type="video/mp4" />
-        </video>
+        <div className="background-image" style={{ backgroundImage: `url(${image})`, backgroundPosition: 'top' }}></div>
       </div>
     );
   }
