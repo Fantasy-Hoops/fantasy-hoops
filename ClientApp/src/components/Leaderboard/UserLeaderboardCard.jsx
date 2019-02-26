b95843bcb4fdbbde127a03768582bd9558e50b33
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-02-27 01:50:43 +0200

    Improve daily UserLeaderboardCard

--- ClientApp/src/components/Leaderboard/UserLeaderboardCard.jsx
+++ ClientApp/src/components/Leaderboard/UserLeaderboardCard.jsx
@@ -12,35 +12,140 @@ export class UserLeaderboardCard extends Component {
 
   async componentWillMount() {
     this.setState({
-      avatar: await loadImage(`${process.env.REACT_APP_IMAGES_SERVER_NAME}/content/images/avatars/${this.props.userid}.png`, defaultPhoto)
+      avatar: await loadImage(`${process.env.REACT_APP_IMAGES_SERVER_NAME}/content/images/avatars/${this.props.user.id}.png`, defaultPhoto)
     });
   }
 
   render() {
-    return (
-      <div className="card bg-white rounded mt-1 mx-auto" style={{ width: '20rem', height: '4.5rem' }}>
-        <div className="card-body">
-          <div className="d-inline-block align-middle mr-1">
-            <h4>{this.props.index + 1}</h4>
-          </div>
-          <a href={`/profile/${this.props.userName}`} >
-            <div className="d-inline-block position-absolute ml-3" style={{ top: '0.2rem' }}>
+    if (this.props.isDaily) {
+      return (
+        <div className="UserLeaderboardCard UserLeaderboardCard--daily card bg-white rounded mt-1 mx-auto">
+          <div className="UserLeaderboardCard__body--daily card-body">
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__user-position">
+              <p>{this.props.index + 1}</p>
+            </div>
+            <div className="UserLeaderboardCard__body-item">
               <Img
-                className="user-card-player"
-                alt={this.props.userName}
+                className="UserLeaderboardCard__user-photo"
+                alt={this.props.user.userName}
                 src={this.state.avatar}
                 decode={false}
               />
             </div>
-            <div className="d-inline-block">
-              <p className="align-middle player-name" style={{ paddingLeft: '5rem', paddingTop: '0.3rem' }}>{this.props.userName}</p>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__username UserLeaderboardCard__username--daily">{this.props.user.userName}</div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__FP UserLeaderboardCard__FP--daily">
+              <p title="Fantasy Points" style={{ margin: 0 }}>{this.props.user.score} FP</p>
             </div>
-          </a>
-          <div className="d-inline-block float-right" style={{ paddingTop: '0.3rem' }}>
-            <h5>{this.props.fp.toFixed(1)} FP</h5>
+            <div className="UserLeaderboardCard__body-item">
+            </div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__player">
+              <div className="UserLeaderboardCard__player-photo--background" style={{ backgroundColor: this.props.user.pg.teamColor }}>
+                <Img
+                  className="UserLeaderboardCard__player-photo--image"
+                  alt={this.props.user.userName}
+                  src={[`http://fantasyhoops.org/content/images/players/${this.props.user.pg.nbaID}.png`]}
+                  decode={false}
+                />
+              </div>
+              <p className="UserLeaderboardCard__player-lastname">
+                {this.props.user.pg.lastName}
+              </p>
+              <p className="UserLeaderboardCard__player-fp">
+                {this.props.user.pg.fp}
+              </p>
+            </div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__player">
+              <div className="UserLeaderboardCard__player-photo--background" style={{ backgroundColor: this.props.user.sg.teamColor }}>
+                <Img
+                  className="UserLeaderboardCard__player-photo--image"
+                  alt={this.props.user.userName}
+                  src={[`http://fantasyhoops.org/content/images/players/${this.props.user.sg.nbaID}.png`]}
+                  decode={false}
+                />
+              </div>
+              <p className="UserLeaderboardCard__player-lastname">
+                {this.props.user.sg.lastName}
+              </p>
+              <p className="UserLeaderboardCard__player-fp">
+                {this.props.user.sg.fp}
+              </p>
+            </div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__player">
+              <div className="UserLeaderboardCard__player-photo--background" style={{ backgroundColor: this.props.user.sf.teamColor }}>
+                <Img
+                  className="UserLeaderboardCard__player-photo--image"
+                  alt={this.props.user.userName}
+                  src={[`http://fantasyhoops.org/content/images/players/${this.props.user.sf.nbaID}.png`]}
+                  decode={false}
+                />
+              </div>
+              <p className="UserLeaderboardCard__player-lastname">
+                {this.props.user.sf.lastName}
+              </p>
+              <p className="UserLeaderboardCard__player-fp">
+                {this.props.user.sf.fp}
+              </p>
+            </div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__player">
+              <div className="UserLeaderboardCard__player-photo--background" style={{ backgroundColor: this.props.user.pf.teamColor }}>
+                <Img
+                  className="UserLeaderboardCard__player-photo--image"
+                  alt={this.props.user.userName}
+                  src={[`http://fantasyhoops.org/content/images/players/${this.props.user.pf.nbaID}.png`]}
+                  decode={false}
+                />
+              </div>
+              <p className="UserLeaderboardCard__player-lastname">
+                {this.props.user.pf.lastName}
+              </p>
+              <p className="UserLeaderboardCard__player-fp">
+                {this.props.user.pf.fp}
+              </p>
+            </div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__player">
+              <div className="UserLeaderboardCard__player-photo--background" style={{ backgroundColor: this.props.user.c.teamColor }}>
+                <Img
+                  className="UserLeaderboardCard__player-photo--image"
+                  alt={this.props.user.userName}
+                  src={[`http://fantasyhoops.org/content/images/players/${this.props.user.c.nbaID}.png`]}
+                  decode={false}
+                />
+              </div>
+              <p className="UserLeaderboardCard__player-lastname">
+                {this.props.user.c.lastName}
+              </p>
+              <p className="UserLeaderboardCard__player-fp">
+                {this.props.user.c.fp}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else
+      return (
+        <div className="UserLeaderboardCard card bg-white rounded mt-1 mx-auto">
+          <div className="UserLeaderboardCard__body card-body">
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__user-position">
+              {this.props.index + 1}
+            </div>
+            <div className="UserLeaderboardCard__body-item">
+              <a href={`/profile/${this.props.user.userName}`} >
+                <Img
+                  className="UserLeaderboardCard__user-photo"
+                  alt={this.props.user.userName}
+                  src={this.state.avatar}
+                  decode={false}
+                />
+              </a>
+            </div>
+
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__username">{this.props.user.userName}</div>
+            <div className="UserLeaderboardCard__body-item UserLeaderboardCard__FP">
+              {this.props.user.score} FP
+            </div>
+          </div>
+        </div>
+      );
   }
 }
