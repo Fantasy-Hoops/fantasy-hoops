dc4a7a2c6698ce946133ab929e46528c8b6ea2d8
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-03-08 01:22:30 +0200

    Working Friend Request PushNotifications

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,8 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { registerPush } from './utils/push';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -15,5 +13,3 @@ ReactDOM.render(
     <App />
   </BrowserRouter>,
   rootElement);
-
-registerPush();
