9495d1f290f2cb713933cc2fd73c995ec49bf98c
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-03-10 17:48:50 +0200

    A2HS support

--- ClientApp/public/sw.js
+++ ClientApp/public/sw.js
@@ -1,37 +1,56 @@
-'use strict';
+"use strict";
 
-importScripts('./js/util.js');
+importScripts("./js/util.js");
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open('static').then(function (cache) {
-      return cache.addAll([
-        '.',
-        '/static/js/main.8b8a6c25.js',
-        '/static/css/main.d978d13d.css',
-        'static/media/favicon.f3a2aac4.ico',
-        'favicon.ico'
-      ]).then(() => self.skipWaiting());
+self.addEventListener("install", function(event) {
+  var offlineRequest = new Request("./offline");
+  event.waitUntil(
+    fetch(offlineRequest).then(function(response) {
+      return caches.open("offline").then(function(cache) {
+        console.log("[oninstall] Cached offline page", response.url);
+        return cache.put(offlineRequest, response);
+      });
     })
   );
 });
 
-self.addEventListener('fetch', function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", function(event) {
+  var request = event.request;
+  if (request.method === "GET") {
+    event.respondWith(
+      fetch(request).catch(function(error) {
+        console.error(
+          "[onfetch] Failed. Serving cached offline fallback " + error
+        );
+        return caches.open("offline").then(function(cache) {
+          return cache.match("./offline");
+        });
+      })
+    );
+  }
 });
 
-self.addEventListener('activate', function (event) {
+self.addEventListener("activate", function(event) {
   event.waitUntil(clients.claim());
 });
 
 // Respond to a server push with a user notification
-self.addEventListener('push', function (event) {
+self.addEventListener("push", function(event) {
   if (event.data) {
-    const { title, lang = 'en', body, tag, timestamp, requireInteraction, actions, image, icon, data, badge, vibrate } = event.data.json();
+    const {
+      title,
+      lang = "en",
+      body,
+      tag,
+      timestamp,
+      requireInteraction,
+      actions,
+      image,
+      icon,
+      data,
+      badge,
+      vibrate
+    } = event.data.json();
     const promiseChain = self.registration.showNotification(title, {
       lang,
       body,
@@ -40,9 +59,9 @@ self.addEventListener('push', function (event) {
       timestamp: timestamp ? Date.parse(timestamp) : undefined,
       actions: actions || undefined,
       image: image || undefined,
-      icon: icon || './favicon.ico',
+      icon: icon || "./favicon.ico",
       data: data || null,
-      badge: badge || '../public/images/notificationBadge.png',
+      badge: badge || "../public/images/notificationBadge.png",
       vibrate: vibrate || null
     });
     // Ensure the toast notification is displayed before exiting this function
@@ -50,62 +69,72 @@ self.addEventListener('push', function (event) {
   }
 });
 
-self.addEventListener('notificationclick', async (event) => {
-  const model = event.notification.data;
-  event.notification.close();
-  switch (event.action) {
-    case 'accept':
-      await fetch('/api/friendrequest/accept', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(model)
-      }).then(async () => {
-        const notification = {
-          title: "Fantasy Hoops Friend Request",
-          body: `User '${model.receiverUsername}' accepted your friend request!`,
-          icon: `https://fantasyhoops.org/content/images/avatars/${model.receiverID}.png`
-        };
-        await fetch(`./api/push/send/${model.senderID}`, {
-          method: 'post',
+self.addEventListener(
+  "notificationclick",
+  async event => {
+    const model = event.notification.data;
+    event.notification.close();
+    switch (event.action) {
+      case "accept":
+        await fetch("/api/friendrequest/accept", {
+          method: "POST",
           headers: {
-            'Content-type': 'application/json'
+            "Content-type": "application/json"
           },
-          body: JSON.stringify(notification)
+          body: JSON.stringify(model)
+        })
+          .then(async () => {
+            const notification = {
+              title: "Fantasy Hoops Friend Request",
+              body: `User '${
+                model.receiverUsername
+              }' accepted your friend request!`,
+              icon: `https://fantasyhoops.org/content/images/avatars/${
+                model.receiverID
+              }.png`
+            };
+            await fetch(`./api/push/send/${model.senderID}`, {
+              method: "post",
+              headers: {
+                "Content-type": "application/json"
+              },
+              body: JSON.stringify(notification)
+            });
+          })
+          .then(clients.openWindow(`/profile/${model.senderUsername}`));
+        break;
+      case "decline":
+        await fetch("/api/friendrequest/cancel", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify(model)
         });
-      }).then(clients.openWindow(`/profile/${model.senderUsername}`));
-      break;
-    case 'decline':
-      await fetch('/api/friendrequest/cancel', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(model)
-      })
-      break;
-    case 'lineup':
-      clients.openWindow("/lineup");
-      break;
-    case 'leaderboard':
-      clients.openWindow("/leaderboard/users");
-      break;
-    case 'new_user':
-      clients.openWindow(`/profile/${model.userName}`)
-      break;
-    default:
-      clients.openWindow("/");
-  }
-}, false);
+        break;
+      case "lineup":
+        clients.openWindow("/lineup");
+        break;
+      case "leaderboard":
+        clients.openWindow("/leaderboard/users");
+        break;
+      case "new_user":
+        clients.openWindow(`/profile/${model.userName}`);
+        break;
+      default:
+        clients.openWindow("/");
+    }
+  },
+  false
+);
 
-
-self.addEventListener('notificationclick', function (event) {
+self.addEventListener("notificationclick", function(event) {
   event.notification.close();
 
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true })
-      .then(function (clientList) {
+    clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then(function(clientList) {
         if (clientList.length > 0) {
           let client = clientList[0];
 
@@ -117,17 +146,24 @@ self.addEventListener('notificationclick', function (event) {
           return client.focus();
         }
 
-        return clients.openWindow('/');
+        return clients.openWindow("/");
       })
   );
 });
 
-self.addEventListener('pushsubscriptionchange', function (event) {
+self.addEventListener("pushsubscriptionchange", function(event) {
   event.waitUntil(
     Promise.all([
-      Promise.resolve(event.oldSubscription ? deleteSubscription(event.oldSubscription) : true),
-      Promise.resolve(event.newSubscription ? event.newSubscription : subscribePush(registration))
-        .then(function (sub) { return saveSubscription(sub); })
+      Promise.resolve(
+        event.oldSubscription ? deleteSubscription(event.oldSubscription) : true
+      ),
+      Promise.resolve(
+        event.newSubscription
+          ? event.newSubscription
+          : subscribePush(registration)
+      ).then(function(sub) {
+        return saveSubscription(sub);
+      })
     ])
   );
 });
--- ClientApp/src/components/Main.jsx
+++ ClientApp/src/components/Main.jsx
@@ -1,57 +1,59 @@
-import React, { Component } from 'react';
-import { isAuth, parse } from '../utils/auth';
+import React, { Component } from "react";
+import { isAuth, parse } from "../utils/auth";
 
 export class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      team: ''
-    }
+      team: ""
+    };
   }
 
   async componentWillMount() {
     if (isAuth()) {
       const user = parse();
-      await fetch(`${process.env.REACT_APP_SERVER_NAME}/api/user/team/${user.id}`)
+      await fetch(
+        `${process.env.REACT_APP_SERVER_NAME}/api/user/team/${user.id}`
+      )
         .then(res => res.json())
         .then(res => {
           const team = res[0].team;
           this.setState({
-            team: team,
+            team: team
           });
         });
     } else {
       this.setState({
-        team: { abbreviation: 'nba' }
-      })
+        team: { abbreviation: "nba" }
+      });
     }
   }
 
   componentDidMount() {
     let deferredPrompt;
-    const addBtn = document.querySelector('.A2HS-Button');
-    addBtn.style.display = 'none';
 
-    window.addEventListener('beforeinstallprompt', (e) => {
+    window.addEventListener("beforeinstallprompt", e => {
+      const addBtn = document.querySelector(".A2HS-Button");
+      addBtn.style.display = "none";
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
       deferredPrompt = e;
       // Update UI to notify the user they can add to home screen
-      addBtn.style.display = 'block';
+      addBtn.style.display = "block";
 
-      addBtn.addEventListener('click', (e) => {
+      addBtn.addEventListener("click", e => {
         console.log(e);
         // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
+        addBtn.style.display = "none";
         // Show the prompt
         deferredPrompt.prompt();
         // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
+        deferredPrompt.userChoice.then(choiceResult => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("User accepted the A2HS prompt");
           } else {
-            console.log('User dismissed the A2HS prompt');
+            console.log("User dismissed the A2HS prompt");
           }
           deferredPrompt = null;
         });
@@ -60,24 +62,37 @@ export class Main extends Component {
   }
 
   render() {
-    if (this.state.team === '') {
-      return <div></div>;
+    if (this.state.team === "") {
+      return <div />;
     }
     const image = this.state.team
       ? require(`../content/images/backgrounds/${this.state.team.abbreviation.toLowerCase()}.png`)
       : require(`../content/images/backgrounds/nba.png`);
     return (
       <div>
-        <button className="btn btn-success A2HS-Button">Add to home screen</button>
+        <button className="btn btn-success A2HS-Button">
+          Add to home screen
+        </button>
         <h1 id="main-text" className="text-center title">
           Fantasy Hoops
         </h1>
         <div className="text-center">
-          <a href="/lineup" className="btn btn-primary mt-4" role="button" style={{ fontSize: '80%' }}>
+          <a
+            href="/lineup"
+            className="btn btn-primary mt-4"
+            role="button"
+            style={{ fontSize: "80%" }}
+          >
             Play Now!
-            </a>
+          </a>
         </div>
-        <div className="background-image" style={{ backgroundImage: `url(${image})`, backgroundPosition: 'top' }}></div>
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundPosition: "top"
+          }}
+        />
       </div>
     );
   }
