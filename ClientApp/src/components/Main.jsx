c8904d1985d94098ba7ddec4cc161b92a80394e1
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-03-10 12:26:34 +0200

    Changes for A2HS support

--- ClientApp/src/components/Main.jsx
+++ ClientApp/src/components/Main.jsx
@@ -27,6 +27,38 @@ export class Main extends Component {
     }
   }
 
+  componentDidMount() {
+    let deferredPrompt;
+    const addBtn = document.querySelector('.A2HS-Button');
+    addBtn.style.display = 'none';
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      addBtn.style.display = 'block';
+
+      addBtn.addEventListener('click', (e) => {
+        console.log(e);
+        // hide our user interface that shows our A2HS button
+        addBtn.style.display = 'none';
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
+  }
+
   render() {
     if (this.state.team === '') {
       return <div></div>;
@@ -36,6 +68,7 @@ export class Main extends Component {
       : require(`../content/images/backgrounds/nba.png`);
     return (
       <div>
+        <button className="btn btn-success A2HS-Button">Add to home screen</button>
         <h1 id="main-text" className="text-center title">
           Fantasy Hoops
         </h1>
