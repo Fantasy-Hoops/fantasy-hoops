050f71262e15da215c6cee24d24ce3199d143429
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-04-02 22:37:24 +0300

    Injuries improvement

--- Database/InjuriesSeed.cs
+++ Database/InjuriesSeed.cs
@@ -16,145 +16,162 @@ using System.Diagnostics;
 
 namespace fantasy_hoops.Database
 {
-	public class InjuriesSeed
-	{
-		const int DAYS_TO_SAVE = 2;
-		static DateTime dayFrom = DateTime.UtcNow.AddDays(-DAYS_TO_SAVE);
-		private static Stack<InjuryPushNotificationViewModel> lineupsAffected = new Stack<InjuryPushNotificationViewModel>();
+    public class InjuriesSeed
+    {
+        private static Stack<InjuryPushNotificationViewModel> lineupsAffected = new Stack<InjuryPushNotificationViewModel>();
 
-		public static void Initialize(GameContext context)
-		{
-			if (JobManager.RunningSchedules.Any(s => !s.Name.Equals("injuries")))
-			{
-				JobManager.AddJob(() => Initialize(context),
-				s => s.WithName("injuries")
-				.ToRunOnceIn(30)
-				.Seconds());
-				return;
-			}
+        public static void Initialize(GameContext context)
+        {
+            if (JobManager.RunningSchedules.Any(s => !s.Name.Equals("injuries")))
+            {
+                JobManager.AddJob(() => Initialize(context),
+                s => s.WithName("injuries")
+                .ToRunOnceIn(30)
+                .Seconds());
+                return;
+            }
 
-			try
-			{
-				Task.Run(() => Extract(context)).Wait();
-			}
-			catch (AggregateException e)
-			{
-				Debug.WriteLine("Injuries seed exception: " + e.Message);
-			}
-		}
+            try
+            {
+                Task.Run(() => Extract(context)).Wait();
+            }
+            catch (AggregateException e)
+            {
+                Debug.WriteLine("Injuries seed exception: " + e.Message);
+            }
+        }
 
-		private static JArray GetInjuries()
-		{
-			HttpWebResponse webResponse = CommonFunctions.GetResponse("https://www.fantasylabs.com/api/players/news/2/");
-			string myResponse = CommonFunctions.ResponseToString(webResponse);
-			JArray injuries = JArray.Parse(myResponse);
-			return injuries;
-		}
+        private static JArray GetInjuries()
+        {
+            HttpWebResponse webResponse = CommonFunctions.GetResponse("https://www.fantasylabs.com/api/players/news/2/");
+            string myResponse = CommonFunctions.ResponseToString(webResponse);
+            JArray injuries = JArray.Parse(myResponse);
+            return injuries;
+        }
 
-		private static async Task Extract(GameContext context)
-		{
-			await context.Injuries.ForEachAsync(inj => context.Injuries.Remove(inj));
-			JArray injuries = GetInjuries();
-			foreach (JObject injury in injuries)
-			{
-				DateTime dateUTC = CommonFunctions.EasternToUTC(DateTime.Parse(injury["ModifiedDate"].ToString())).AddHours(-1);
-				try
-				{
-					if (dayFrom.CompareTo(dateUTC) > 0)
-						break;
-					await AddToDatabaseAsync(context, injury, dateUTC);
-				}
-				catch (Exception)
-				{
-					continue;
-				}
-			}
-			await context.SaveChangesAsync();
-			await SendPushNotifications(context);
-		}
+        private static async Task Extract(GameContext context)
+        {
+            JArray injuries = GetInjuries();
+            foreach (JObject injury in injuries
+                .Where(inj => inj.Value<string>("ModifiedDate") == null
+                    || DateTime.Parse(inj.Value<string>("ModifiedDate")).Year >= int.Parse(CommonFunctions.GetSeasonYear())))
+            {
+                int NbaID;
+                if (injury.Value<int?>("PrimarySourceKey") == null)
+                    continue;
+                NbaID = (int)injury["PrimarySourceKey"];
 
-		private static async Task AddToDatabaseAsync(GameContext context, JToken injury, DateTime dateUTC)
-		{
-			var injuryObj = new Injuries
-			{
-				Title = (string)injury["Title"],
-				Status = (string)injury["PlayerStatus"],
-				Injury = (string)injury["Injury"],
-				Description = (string)injury["News"],
-				Date = dateUTC,
-				Link = (string)injury["Link"]
-			};
-			injuryObj.Player = context.Players.Where(x => x.NbaID == (int)injury["PrimarySourceKey"]).FirstOrDefault();
+                if (context.Injuries
+                    .Any(inj => inj.Player.NbaID == NbaID
+                        && inj.Status.Equals((string)injury["PlayerStatus"])))
+                    continue;
 
-			if (injuryObj.Player == null)
-				return;
-			await context.Injuries.AddAsync(injuryObj);
-			string statusBefore = context.Players
-							.Where(p => p.NbaID == injuryObj.Player.NbaID)
-							.FirstOrDefault()
-							.Status;
-			string statusAfter = injuryObj.Status;
-			context.Players
-							.Where(p => p.NbaID == injuryObj.Player.NbaID)
-							.FirstOrDefault()
-							.Status = injuryObj.Status;
-			context.Players
-							.Where(p => p.NbaID == injuryObj.Player.NbaID)
-							.FirstOrDefault()
-							.StatusDate = dateUTC;
+                DateTime? dateUTC = injury.Value<string>("ModifiedDate") != null
+                    ? CommonFunctions.EasternToUTC(DateTime.Parse(injury["ModifiedDate"].ToString())).AddHours(-1)
+                    : (DateTime?)null;
+                try
+                {
+                    await AddToDatabaseAsync(context, injury, dateUTC);
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+            await context.SaveChangesAsync();
+            await SendPushNotifications(context);
+        }
 
-			if (!statusBefore.Equals(statusAfter))
-				await UpdateNotifications(context, injuryObj, statusBefore, statusAfter);
-		}
+        private static async Task AddToDatabaseAsync(GameContext context, JToken injury, DateTime? dateUTC)
+        {
+            Player injuryPlayer = context.Players.Where(x => x.NbaID == (int)injury["PrimarySourceKey"]).FirstOrDefault();
 
-		private static async Task UpdateNotifications(GameContext context, Injuries injury, string statusBefore, string statusAfter)
-		{
-			foreach (var lineup in context.UserLineups
-							.Where(x => x.Date.Equals(CommonFunctions.UTCToEastern(NextGame.NEXT_GAME).Date)
-							&& (x.PgID == injury.PlayerID
-									|| x.SgID == injury.PlayerID
-									|| x.SfID == injury.PlayerID
-									|| x.PfID == injury.PlayerID
-									|| x.CID == injury.PlayerID)))
-			{
-				lineupsAffected.Push(new InjuryPushNotificationViewModel
-				{
-					UserID = lineup.UserID,
-					StatusBefore = statusBefore,
-					StatusAfter = statusAfter,
-					FullName = injury.Player.FullName,
-					PlayerNbaID = injury.Player.NbaID
-				});
-				var inj = new InjuryNotification
-				{
-					UserID = lineup.UserID,
-					ReadStatus = false,
-					DateCreated = DateTime.UtcNow,
-					PlayerID = injury.PlayerID,
-					InjuryStatus = injury.Status,
-					InjuryDescription = injury.Injury
-				};
+            if (injuryPlayer == null)
+                return;
 
-				if (!context.InjuryNotifications
-				.Any(x => x.InjuryStatus.Equals(inj.InjuryStatus)
-																				&& x.PlayerID == inj.PlayerID))
-					await context.InjuryNotifications.AddAsync(inj);
-			}
-		}
+            var injuryObj = new Injuries
+            {
+                Title = injury.Value<string>("Title") != null ? (string)injury["Title"] : null,
+                Status = injury.Value<string>("PlayerStatus") != null ? (string)injury["PlayerStatus"] : null,
+                Injury = injury.Value<string>("Injury") != null ? (string)injury["Injury"] : null,
+                Description = injury.Value<string>("News") != null ? (string)injury["News"] : null,
+                Date = dateUTC,
+                Link = injury.Value<string>("Link") != null ? (string)injury["Link"] : null
+            };
+            injuryObj.Player = injuryPlayer;
 
-		private static async Task SendPushNotifications(GameContext context)
-		{
-			WebPushClient _webPushClient = new WebPushClient();
-			while (lineupsAffected.Count > 0)
-			{
-				var lineup = lineupsAffected.Pop();
-				PushNotificationViewModel notification =
-								new PushNotificationViewModel(lineup.FullName,
-												string.Format("Status changed from {0} to {1}!", lineup.StatusBefore, lineup.StatusAfter));
-				notification.Image = Environment.GetEnvironmentVariable("IMAGES_SERVER_NAME") + "/content/images/players/" + lineup.PlayerNbaID + ".png";
-				notification.Actions = new List<NotificationAction> { new NotificationAction("lineup", "🤾🏾‍♂️ Lineup") };
-				await PushService.Instance.Value.Send(lineup.UserID, notification);
-			}
-		}
-	}
+            var dbInjury = context.Injuries
+                    .Where(inj => inj.Player.NbaID == (int)injury["PrimarySourceKey"])
+                    .FirstOrDefault();
+
+
+            if (dbInjury == null)
+            {
+                await context.Injuries.AddAsync(injuryObj);
+            }
+            else
+            {
+                dbInjury = injuryObj;
+            }
+
+            string statusBefore = injuryPlayer.Status;
+            string statusAfter = injuryObj.Status;
+
+            injuryPlayer.Status = injuryObj.Status;
+            injuryPlayer.StatusDate = dateUTC;
+
+            if (!statusBefore.Equals(statusAfter))
+                await UpdateNotifications(context, injuryObj, statusBefore, statusAfter);
+        }
+
+        private static async Task UpdateNotifications(GameContext context, Injuries injury, string statusBefore, string statusAfter)
+        {
+            foreach (var lineup in context.UserLineups
+                            .Where(x => x.Date.Equals(CommonFunctions.UTCToEastern(NextGame.NEXT_GAME).Date)
+                            && (x.PgID == injury.PlayerID
+                                    || x.SgID == injury.PlayerID
+                                    || x.SfID == injury.PlayerID
+                                    || x.PfID == injury.PlayerID
+                                    || x.CID == injury.PlayerID)))
+            {
+                lineupsAffected.Push(new InjuryPushNotificationViewModel
+                {
+                    UserID = lineup.UserID,
+                    StatusBefore = statusBefore,
+                    StatusAfter = statusAfter,
+                    FullName = injury.Player.FullName,
+                    PlayerNbaID = injury.Player.NbaID
+                });
+                var inj = new InjuryNotification
+                {
+                    UserID = lineup.UserID,
+                    ReadStatus = false,
+                    DateCreated = DateTime.UtcNow,
+                    PlayerID = injury.PlayerID,
+                    InjuryStatus = injury.Status,
+                    InjuryDescription = injury.Injury
+                };
+
+                if (!context.InjuryNotifications
+                .Any(x => x.InjuryStatus.Equals(inj.InjuryStatus)
+                                                                                && x.PlayerID == inj.PlayerID))
+                    await context.InjuryNotifications.AddAsync(inj);
+            }
+        }
+
+        private static async Task SendPushNotifications(GameContext context)
+        {
+            WebPushClient _webPushClient = new WebPushClient();
+            while (lineupsAffected.Count > 0)
+            {
+                var lineup = lineupsAffected.Pop();
+                PushNotificationViewModel notification =
+                                new PushNotificationViewModel(lineup.FullName,
+                                                string.Format("Status changed from {0} to {1}!", lineup.StatusBefore, lineup.StatusAfter));
+                notification.Image = Environment.GetEnvironmentVariable("IMAGES_SERVER_NAME") + "/content/images/players/" + lineup.PlayerNbaID + ".png";
+                notification.Actions = new List<NotificationAction> { new NotificationAction("lineup", "🤾🏾‍♂️ Lineup") };
+                await PushService.Instance.Value.Send(lineup.UserID, notification);
+            }
+        }
+    }
 }
