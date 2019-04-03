75984be30648e08c361c9cae43b33dc77b2f6ee3
Benas Nekrošius <benasnek@gmail.com>
2019-04-03 23:08:11 +0300

    Add game object. Resovles #52

---
+++ Models/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fantasy_hoops.Models
+{
+    public class Game
+    {
+
+        [Key]
+        public int GameID { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public int HomeTeamID { get; set; }
+
+        public int HomeScore { get; set; }
+
+        public int AwayTeamID { get; set; }
+
+        public int AwayScore { get; set; }
+
+        [ForeignKey("HomeTeamID")]
+        public virtual Team HomeTeam { get; set; }
+
+        [ForeignKey("AwayTeamID")]
+        public virtual Team AwayTeam { get; set; }
+
+
+    }
+}
