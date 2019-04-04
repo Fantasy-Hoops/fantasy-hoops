eaf2d4a4782e0610b32bf67bb01db8b371ff312d
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2019-04-04 13:31:17 +0300

    Collapsable new post form

--- ClientApp/src/app/components/Blog/BlogForm.jsx
+++ ClientApp/src/app/components/Blog/BlogForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 import {
 	Formik, Form, Field
 } from 'formik';
@@ -10,62 +11,84 @@ const author = parse();
 
 const BlogForm = props => (
 	<>
-		<h1 className="text-center">New Post</h1>
-		<div className="form-group">
-			<input className="form-control" disabled value={author.username} />
+		<p>
+			<button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+				<span>
+					<i className="fas fa-pencil-alt" />
+					{' New Post'}
+				</span>
+			</button>
+		</p>
+		<div className="collapse" id="collapseExample">
+			<div className="form-group">
+				<label htmlFor="post-author">Author</label>
+				<input className="form-control" id="post-author" disabled value={author.username} />
+			</div>
+			<Formik
+				validationSchema={blogValidation}
+				onSubmit={(values, actions) => {
+					actions.setSubmitting(true);
+					const { savePost } = props;
+					savePost({ title: values.title, body: values.body, authorID: author.id });
+					actions.resetForm({});
+					actions.setSubmitting(false);
+				}}
+				render={(formProps) => {
+					const {
+						errors,
+						values
+					} = formProps;
+					return (
+						<Form>
+							<div className="form-group">
+								<label htmlFor="post-title">Title</label>
+								<Field
+									className={`form-control ${errors.title && 'is-invalid'}`}
+									id="post-title"
+									component="input"
+									type="text"
+									error={errors.title}
+									name="title"
+									placeholder="Post title"
+									value={values.title ? values.title : ''}
+								/>
+								<small id="title-error" className="form-text text-danger">
+									{errors.title}
+								</small>
+							</div>
+							<div className="form-group">
+								<label htmlFor="post-body">Body</label>
+								<Field
+									className={`form-control ${errors.body && 'is-invalid'}`}
+									id="post-body"
+									component="textarea"
+									rows="10"
+									type="text"
+									name="body"
+									placeholder="Post body"
+									value={values.body ? values.body : ''}
+								/>
+								<small id="body-error" className="form-text text-danger">
+									{errors.body}
+								</small>
+							</div>
+							<button
+								type="submit"
+								className="btn btn-outline-info btn-block"
+								disabled={!_.isEmpty(errors) || _.isEmpty(values)}
+							>
+								{'Submit'}
+							</button>
+						</Form>
+					);
+				}}
+			/>
 		</div>
-		<Formik
-			validationSchema={blogValidation}
-			onSubmit={(values, actions) => {
-				actions.setSubmitting(true);
-				const { savePost } = props;
-				savePost({ title: values.title, body: values.body, authorID: author.id });
-				actions.resetForm({ title: '', body: '' });
-				actions.setSubmitting(false);
-			}}
-			render={(props) => {
-				const {
-					errors,
-					values,
-					isSubmitting
-				} = props;
-				return (
-					<Form>
-						<div className="form-group">
-							<Field
-								className="form-control"
-								component="input"
-								type="text"
-								error={errors.title}
-								name="title"
-								placeholder="Post title"
-								value={values.title ? values.title : ''}
-							/>
-						</div>
-						<div className="form-group">
-							<Field
-								className="form-control"
-								component="textarea"
-								rows="10"
-								type="text"
-								error={errors.body}
-								name="body"
-								placeholder="Post body"
-								value={values.body ? values.body : ''}
-							/>
-						</div>
-						<button
-							type="submit"
-							className="btn btn-outline-success btn-block"
-							disabled={isSubmitting}
-						>
-							{'Submit'}
-						</button>
-					</Form>
-				);
-			}}
-		/>
 	</>
 );
+
+BlogForm.propTypes = {
+	savePost: PropTypes.func.isRequired
+};
 
 export default BlogForm;
--- ClientApp/src/app/components/Blog/PostCard.jsx
+++ ClientApp/src/app/components/Blog/PostCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import Img from 'react-image';
 import defaultPhoto from '../../../content/images/default.png';
 
 const PostCard = (props) => {
   const { post } = props;
-  console.log(post);
   return (
-    <div className="NewsCard mt-5 mx-auto news-card card">
+    <div className="card mt-5">
       <div className="card-header bg-primary text-white">
         <h3 className="card-title">
           {post.title}
@@ -15,7 +15,7 @@ const PostCard = (props) => {
       </div>
       <div className="card-header text-muted">
         <Img
-          className="Blog__AuthorImage"
+          className="PostCard__AuthorImage"
           alt=""
           src={[
             `${process.env.REACT_APP_IMAGES_SERVER_NAME}/content/images/avatars/${post.author.id}.png`,
@@ -28,11 +28,22 @@ const PostCard = (props) => {
           {moment(post.createdAt).format('MM/DD/YYYY HH:mm')}
         </span>
       </div>
-      <div className="card-body">
+      <div className="card-body" style={{ whiteSpace: 'pre-line' }}>
         {post.body}
       </div>
     </div>
   );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    author: PropTypes.shape({
+      userName: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 };
 
 export default PostCard;
