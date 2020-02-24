b5349d0032510e871060084a1292fabde57f63cf
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2020-02-24 23:48:03 +0200

    Add price below BLC FP

--- ClientApp/src/app/components/Profile/UserScore.jsx
+++ ClientApp/src/app/components/Profile/UserScore.jsx
@@ -21,7 +21,7 @@ export class UserScore extends PureComponent {
         )
       );
     }
-    const { current } = this.props;
+    const { current, activity } = this.props;
     return (
       <div className={`UserScoreCard card bg-white rounded ${this.props.className}`} style={{ width: `${this.props.width}`, margin: `${this.props.center}`, marginBottom: '0.5rem' }}>
         <div className="UserScoreCard__body card-body">
@@ -32,10 +32,13 @@ export class UserScore extends PureComponent {
                 ? _.map(this.props.activity.lineup, lineup => (lineup.fp))
                   .reduce((a, b) => a + b).toFixed(1)
                 : this.props.activity.fp.toFixed(1)}
-              <span style={{ fontSize: '1rem', fontWeight: 400, marginBottom: '0rem' }}>
+              <span style={{ fontSize: '1rem', fontWeight: 400, margin: '0 0 0 .3rem', display: 'table' }}>
                 {' FP'}
               </span>
               <div className="UserScoreCard__date">{this.props.activity.shortDate}</div>
+            </div>
+            <div>
+              {!current && activity.price ? <span>&#36;{activity.price}K</span> : null}
             </div>
           </div>
         </div>
