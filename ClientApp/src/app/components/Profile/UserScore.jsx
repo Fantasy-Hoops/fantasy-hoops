167ceae6df0057887ca6f1b0f5a0235d92ee6b5c
Mantas Naidzinavičius <naidzinavicius.m@gmail.com>
2020-02-25 07:30:10 +0200

    Fix UserScore styling

--- ClientApp/src/app/components/Profile/UserScore.jsx
+++ ClientApp/src/app/components/Profile/UserScore.jsx
@@ -1,50 +1,52 @@
-import React, { PureComponent } from 'react';
+import React, {PureComponent} from 'react';
 import shortid from 'shortid';
 import _ from 'lodash';
-import { UserScoreCard } from './UserScoreCard';
+import {UserScoreCard} from './UserScoreCard';
 
 export class UserScore extends PureComponent {
-  render() {
-    let players = '';
-    if (this.props.activity != null) {
-      players = _.map(
-        this.props.activity.lineup,
-        player => (
-          <UserScoreCard
-            key={shortid()}
-            player={player.player}
-            teamColor={player.teamColor}
-            fp={player.fp}
-            price={player.price}
-            showModal={this.props.showModal}
-          />
-        )
-      );
+    render() {
+        let players = '';
+        if (this.props.activity != null) {
+            players = _.map(
+                this.props.activity.lineup,
+                player => (
+                    <UserScoreCard
+                        key={shortid()}
+                        player={player.player}
+                        teamColor={player.teamColor}
+                        fp={player.fp}
+                        price={player.price}
+                        showModal={this.props.showModal}
+                    />
+                )
+            );
+        }
+        const {current, activity} = this.props;
+        return (
+            <div className={`UserScoreCard card bg-white rounded ${this.props.className}`}
+                 style={{width: `${this.props.width}`, margin: `${this.props.center}`, marginBottom: '0.5rem'}}>
+                <div className="UserScoreCard__body card-body">
+                    {players}
+                    <div className="UserScoreCard__body-item UserScoreCard__result">
+                        <div className="UserScoreCard__FP">
+                            {current
+                                ? _.map(this.props.activity.lineup, lineup => (lineup.fp))
+                                    .reduce((a, b) => a + b).toFixed(1)
+                                : this.props.activity.fp.toFixed(1)}
+                            <span style={{fontSize: '1rem', fontWeight: 400, margin: '0 0 0 .3rem', display: 'table'}}>
+                                {' FP'}
+                            </span>
+                        </div>
+                        <div>
+                            {!current && activity.price ? <span>&#36;{activity.price}K</span> : null}
+                        </div>
+                        {!this.props.activity.shortdate ?
+                            <div className="UserScoreCard__date">{this.props.activity.shortDate}</div> : null}
+                    </div>
+                </div>
+            </div>
+        );
     }
-    const { current, activity } = this.props;
-    return (
-      <div className={`UserScoreCard card bg-white rounded ${this.props.className}`} style={{ width: `${this.props.width}`, margin: `${this.props.center}`, marginBottom: '0.5rem' }}>
-        <div className="UserScoreCard__body card-body">
-          {players}
-          <div className="UserScoreCard__body-item UserScoreCard__result">
-            <div className="UserScoreCard__FP">
-              {current
-                ? _.map(this.props.activity.lineup, lineup => (lineup.fp))
-                  .reduce((a, b) => a + b).toFixed(1)
-                : this.props.activity.fp.toFixed(1)}
-              <span style={{ fontSize: '1rem', fontWeight: 400, margin: '0 0 0 .3rem', display: 'table' }}>
-                {' FP'}
-              </span>
-              <div className="UserScoreCard__date">{this.props.activity.shortDate}</div>
-            </div>
-            <div>
-              {!current && activity.price ? <span>&#36;{activity.price}K</span> : null}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
 }
 
 export default UserScore;
