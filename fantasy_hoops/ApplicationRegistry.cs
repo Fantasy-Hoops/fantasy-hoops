b53ac7fdf50fdae51e0117e051a4cc4ff3807bf5
Naidze <naidzinavicius.m@gmail.com>
2020-08-05 17:44:43 +0300

    Revert ApplicationRegistry

--- fantasy_hoops/ApplicationRegistry.cs
+++ fantasy_hoops/ApplicationRegistry.cs
@@ -13,30 +13,30 @@ namespace fantasy_hoops
     {
         public ApplicationRegistry(GameContext context, IScoreService scoreService, IPushService pushService)
         {
-            // if (context.Teams.Count() < 30)
+            if (context.Teams.Count() < 30)
                 Schedule(new RostersJob(pushService))
                     .WithName("rosterJob")
                     .ToRunNow();
 
-            // Task.Run(() => RostersJob.UpdateTeamColors(new GameContext()));
-            //
-            // Schedule(new NextGameJob(scoreService, pushService, false))
-            //     .WithName("nextGameJob")
-            //     .ToRunNow();
-            // RuntimeUtils.NEXT_GAME_CLIENT = RuntimeUtils.NEXT_GAME;
-            // RuntimeUtils.PLAYER_POOL_DATE = RuntimeUtils.NEXT_GAME;
-            //
-            // Schedule(new InjuriesJob(pushService))
-            //     .WithName("injuriesJob")
-            //     .ToRunNow()
-            //     .AndEvery(10)
-            //     .Minutes();
-            //
-            // Schedule(new PhotosJob())
-            //     .WithName("photosJob")
-            //     .ToRunEvery(1)
-            //     .Days()
-            //     .At(00, 04);
+            Task.Run(() => RostersJob.UpdateTeamColors(new GameContext()));
+            
+            Schedule(new NextGameJob(scoreService, pushService, false))
+                .WithName("nextGameJob")
+                .ToRunNow();
+            RuntimeUtils.NEXT_GAME_CLIENT = RuntimeUtils.NEXT_GAME;
+            RuntimeUtils.PLAYER_POOL_DATE = RuntimeUtils.NEXT_GAME;
+            
+            Schedule(new InjuriesJob(pushService))
+                .WithName("injuriesJob")
+                .ToRunNow()
+                .AndEvery(10)
+                .Minutes();
+            
+            Schedule(new PhotosJob())
+                .WithName("photosJob")
+                .ToRunEvery(1)
+                .Days()
+                .At(00, 04);
         }
     }
 }
